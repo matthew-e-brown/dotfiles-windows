fceab1f6bc69f718baf9309e905c1301f6fe6e0d
Matthew Brown <matthew.e.brown.17@gmail.com>
2026-07-10 22:30:25 -0400

    flac-exchange.py: Change -o/--overwrite to -f/--force
    Also do some formatting/tidying.

--- Scripts/flac-exchange.py
+++ Scripts/flac-exchange.py
@@ -11,24 +11,29 @@ from subprocess import run
 from typing import Iterable
 
 
-def main(out_type: str, src_dirs: Iterable[str], overwrite: bool = False, verbose: bool = False) -> int:
+def main(
+    out_type: str,
+    src_dirs: Iterable[str],
+    force: bool = False,
+    verbose: bool = False
+) -> int:
     """
     Recursively convert a directory of FLAC files to either MP3 or AIFF to be imported into iTunes.
 
     This script duplicates the target directories before converting their contents (the original
     files are untouched).
 
-    :param str out_type:            The to convert FLAC into, either MP3 or AIFF.
+    :param str out_type:            The format to convert FLAC into, either MP3 or AIFF.
     :param Iterable[str] src_dirs:  One or more directories to clone and convert.
-    :param bool overwrite:          Whether or not output directories should be overwritten if they already exist.
+    :param bool force:              Whether or not output directories should be overwritten if they already exist.
     :param bool verbose:            If FFMpeg's stdout (technically, stderr) should be visible.
     :rtype:                         int
     :return:                        A status code; 0 for success, 1 for failure
     """
 
     # Strip trailing slashes, which can confuse glob
-    src_dirs = [ s.rstrip('\\/') for s in src_dirs ]
-    out_dirs = [ join(dirname(d), f"[{out_type.upper()}] {basename(d)}") for d in src_dirs ]
+    src_dirs = [s.rstrip('\\/') for s in src_dirs]
+    out_dirs = [join(dirname(d), f"[{out_type.upper()}] {basename(d)}") for d in src_dirs]
 
     for src, out in zip(src_dirs, out_dirs):
         print(f"\nCopying \"{src}\" to \"{out}\"")
@@ -40,10 +45,10 @@ def main(out_type: str, src_dirs: Iterable[str], overwrite: bool = False, verbos
                 copytree(src, out)
                 done = True
             except FileExistsError:
-                file = stdout if overwrite else stderr
+                file = stdout if force else stderr
                 print(f"    \"{out}\" already exists", file=file)
 
-                if overwrite:
+                if force:
                     try:
                         rmtree(out)
                     except PermissionError as err:
@@ -51,21 +56,21 @@ def main(out_type: str, src_dirs: Iterable[str], overwrite: bool = False, verbos
                         print("\nMaybe try it again?")
                         return 1
                 else:
-                    print("\nRefusing to overwrite output directory without overwrite flag.", file=stderr)
+                    print("\nRefusing to overwrite output directory without force flag.", file=stderr)
                     return 1
         del done
 
         print("\nSearching copied directory for *.flac files...")
 
         flac_files = glob(f"{escape(out)}/**/*.flac", recursive=True)
-        dest_files = [ f"{splitext(f)[0]}.{out_type.lower()}" for f in flac_files ]
+        dest_files = [f"{splitext(f)[0]}.{out_type.lower()}" for f in flac_files]
 
         # Determine what options to pass to FFMpeg
-        options = [ ]
+        options = []
         if out_type == 'aiff':
-            options += [ '-write_id3v2', '1', '-c:a', 'pcm_s16be' ]
+            options += ['-write_id3v2', '1', '-c:a', 'pcm_s16be']
         elif out_type == 'mp3':
-            options += [ '-ab', '320k', '-c:a', 'mp3' ]
+            options += ['-ab', '320k', '-c:a', 'mp3']
 
         print("\nConverting *.flac files, using the command:")
         print(f"ffmpeg -i ... {' '.join(options)} -c:v copy ...\n")
@@ -74,11 +79,11 @@ def main(out_type: str, src_dirs: Iterable[str], overwrite: bool = False, verbos
         for flac, dest in zip(flac_files, dest_files):
             print(f"    Converting {flac}")
 
-            command = [ 'ffmpeg', '-i', flac, *options, '-c:v', 'copy', dest ]
+            command = ['ffmpeg', '-i', flac, *options, '-c:v', 'copy', dest]
             if not verbose:
-                command += [ '-loglevel', 'error' ]
-            if overwrite:
-                command += [ '-y' ]
+                command += ['-loglevel', 'error']
+            if force:
+                command += ['-y']
 
             run(command)
 
@@ -97,8 +102,9 @@ if __name__ == '__main__':
     from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
     # Generate description text by stripping main's doc-string
-    description = ( line.lstrip() for line in main.__doc__.splitlines() )
-    description = ( line for line in description if not line.strip().startswith(':') )
+    assert main.__doc__ is not None
+    description = (line.lstrip() for line in main.__doc__.splitlines())
+    description = (line for line in description if not line.strip().startswith(':'))
     description = linesep.join(description).strip()
 
     parser = ArgumentParser(
@@ -122,8 +128,8 @@ if __name__ == '__main__':
     )
 
     parser.add_argument(
-        '-o',
-        '--overwrite',
+        '-f',
+        '--force',
         action='store_true',
         required=False,
         help="overwrite the output directories if they already exist"
